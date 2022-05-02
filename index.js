ef1d9cdee8281db7b223cfb64a25f18b59b5ef42
Kenny Clare <kmitchellclare@gmail.com>
2022-05-02 13:05:32 -0700

    commit tree 7835d43e4dd3858301919eaf137a9311c2d6f915

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(array, str) {
+    let driverNames = array.filter(name => name.toLowerCase() === str.toLowerCase())
+    return driverNames
+}
+
+function fuzzyMatch(array, str) {
+    return array.filter(array => array.startsWith(str))
+}
+
+
+function matchName(array, str) {
+    return array.filter(array => array.name === str);
+}
